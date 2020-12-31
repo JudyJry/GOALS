a8ae3f9e329d327e7b5b63718d191ef9b76cef36
USER <judyjry0127@gmail.com>
2020-12-31 12:21:44 +0800

    main

--- app/src/main/java/com/example/test/ui/user/UserFriendFragment.kt
+++ app/src/main/java/com/example/test/ui/user/UserFriendFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.test.R
 import com.example.test.UserInfo
 import org.jetbrains.anko.imageResource
@@ -14,6 +16,7 @@ import org.jetbrains.anko.imageResource
 class UserFriendFragment : Fragment() {
     private lateinit var moreTextView: TextView
     private lateinit var friendImage: MutableList<ImageView>
+    private lateinit var friendYetList: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +25,8 @@ class UserFriendFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_user_friend, container, false)
         setVal(root)
         setFriendImage()
+        friendYetList.layoutManager = LinearLayoutManager(root.context)
+        friendYetList.adapter = FriendYetListAdapter()
         return root
     }
 
@@ -36,6 +41,7 @@ class UserFriendFragment : Fragment() {
             root.findViewById(R.id.friend_now_5),
             root.findViewById(R.id.friend_now_6),
         )
+        friendYetList = root.findViewById(R.id.friend_yet_list)
     }
 
     private fun setFriendImage() {
@@ -70,5 +76,30 @@ class UserFriendFragment : Fragment() {
         } else {for (i in IntRange(0,friendNum-1)) {
             friendImage[i].visibility = View.GONE
         }}
+    }
+
+    class FriendYetListAdapter :
+        RecyclerView.Adapter<FriendYetListAdapter.Holder>() {
+        private var mList: MutableList<Int> = arrayListOf(0,1,2,3,4)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val itemView =
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_user_friend_yet, parent, false)
+            return Holder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.bindData(mList[position])
+        }
+
+        override fun getItemCount(): Int = mList.count()
+
+        class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val item: TextView = itemView.findViewById(R.id.friend_yet_image)
+            fun bindData(it: Int) {
+                item.text = it.toString()
+            }
+        }
     }
 }
