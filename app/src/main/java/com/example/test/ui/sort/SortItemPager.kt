c547eb6236b03a916603e42662f83c52afd45254
USER <judyjry0127@gmail.com>
2021-01-01 13:29:27 +0800

    user page finish

--- app/src/main/java/com/example/test/ui/sort/SortItemPager.kt
+++ app/src/main/java/com/example/test/ui/sort/SortItemPager.kt
@@ -15,7 +15,7 @@ class SortItemPager(private val tabItem: ArrayList<Int>) : RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.bindData()
+        holder.bindData(position)
     }
 
     override fun getItemCount(): Int {
@@ -23,9 +23,26 @@ class SortItemPager(private val tabItem: ArrayList<Int>) : RecyclerView.Adapter<
     }
     class PagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private val listView : RecyclerView = itemView.findViewById(R.id.sort_item_listView)
-        fun bindData() {
+        fun bindData(position: Int) {
+            val list : List<String> = if (position == 0) {
+                arrayListOf(
+                    "初學者學籃球會很難嗎？",
+                    "排球志工招募中",
+                    "台北室內運動場有哪些？",
+                    "左屁股左大腿劇痛",
+                    "球類運動不好的人可以打系上球隊嗎"
+                )
+            } else {
+                arrayListOf(
+                    "左屁股左大腿劇痛",
+                    "球類運動不好的人可以打系上球隊嗎",
+                    "排球志工招募中",
+                    "台北室內運動場有哪些？",
+                    "初學者學籃球會很難嗎？"
+                )
+            }
             listView.layoutManager = LinearLayoutManager(itemView.context)
-            listView.adapter = SortListPost()
+            listView.adapter = SortListPost(list)
         }
     }
 }
--- app/src/main/java/com/example/test/ui/sort/SortListPost.kt
+++ app/src/main/java/com/example/test/ui/sort/SortListPost.kt
@@ -1,14 +1,19 @@
 package com.example.test.ui.sort
 
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test.R
 
-class SortListPost : RecyclerView.Adapter<SortListPost.Holder>() {
-    private var mList = arrayListOf(0,1,2,3,4,5)
+
+class SortListPost(list: List<String>) : RecyclerView.Adapter<SortListPost.Holder>() {
+    private var mList = list
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.item_sort_tab_post, parent, false)
@@ -22,10 +27,38 @@ class SortListPost : RecyclerView.Adapter<SortListPost.Holder>() {
     override fun getItemCount(): Int {
         return mList.count()
     }
-    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val name: TextView = itemView.findViewById(R.id.post_user_name)
-        fun bindData(i: Int){
-            name.text = i.toString()
+
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val title: TextView = itemView.findViewById(R.id.post_title)
+        private val more: ImageButton = itemView.findViewById(R.id.post_more)
+        private val heart: ImageButton = itemView.findViewById(R.id.post_heart)
+        private val heartText: TextView = itemView.findViewById(R.id.post_heart_text)
+        private val save: ImageButton = itemView.findViewById(R.id.post_save)
+        private val saveText: TextView = itemView.findViewById(R.id.post_save_text)
+        fun bindData(i: String) {
+            title.text = i
+            more.setOnClickListener {
+                val popupMenu = PopupMenu(itemView.context, more)
+                val inflater: MenuInflater = popupMenu.menuInflater
+                inflater.inflate(R.menu.sort_post_more, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener {
+                    Toast.makeText(itemView.context, "已檢舉", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+                popupMenu.show()
+            }
+            heart.setOnClickListener {
+                heart.isSelected = !heart.isSelected
+            }
+            heartText.setOnClickListener {
+                heart.isSelected = !heart.isSelected
+            }
+            save.setOnClickListener {
+                save.isSelected = !save.isSelected
+            }
+            saveText.setOnClickListener {
+                save.isSelected = !save.isSelected
+            }
         }
     }
 }
